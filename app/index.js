1c2a14da5aebe684ad2d56cde2b5d162c635b737
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-10-24 19:11:26 +0600

    tailwind css with nativewind setup

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View>
+      <Text className="text-red-700">index</Text>
+      <Text className="text-red-700">nice</Text>
+      <Text className="text-red-700 text-3xl">index</Text>
+      <Text className="text-red-700">index</Text>
+      <Text className="text-red-700">vairevai obosese</Text>
+    </View>
+  );
+};
+
+export default index;
